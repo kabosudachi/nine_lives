843b21b6432ae41b4688f22e7077b082cff5de9f
西村真奈 <kabosudachi86@gmail.com>
2015-07-05 15:28:05 +0900

    Initial Commit

---
+++ NineLives/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NineLives
+//
+//  Created by 西村真奈 on 2015/07/05.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
